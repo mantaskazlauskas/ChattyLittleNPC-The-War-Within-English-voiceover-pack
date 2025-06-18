4b5a61b7c0781de386e0b3f6e6c20ee11aaa6248
Tuubby <mmantas.kazlauskas@gmail.com>
2025-06-18 11:19:54 +0300

    init commit

---
+++ Main.lua
@@ -0,0 +1,9 @@
+---@class ChattyLittleNpc_The_War_Within_voiceovers: table
+local ChattyLittleNpc_The_War_Within_voiceovers = LibStub("AceAddon-3.0"):NewAddon("ChattyLittleNpc_The_War_Within_voiceovers")
+
+ChattyLittleNpc_The_War_Within_voiceovers.Metadata = {
+    ContainsQuestVoiceovers = true,
+    ContainsGossipVoiceovers = true,
+    ContainsGameObjectVoiceovers = true,
+    ContainsItemVoiceovers = true
+}
